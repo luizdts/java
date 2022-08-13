d9ff5a02aafc8815929b8aee123366b63d3621ac
luizdts <luizhadantas@gmail.com>
2022-08-13 17:40:54 -0300

    Project 5 using SpringBoot and another files.

---
+++ src/spring-mongodb/src/main/java/com/springmongodb/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.springmongodb.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springmongodb.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+	
+}
